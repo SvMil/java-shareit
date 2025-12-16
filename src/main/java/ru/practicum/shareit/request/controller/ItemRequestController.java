b6ae9959ae72a43b5245c60f65d9383b39396c80
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2025-12-16 13:34:59 +0300

    add shareit

--- src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.request.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RestController
-@RequestMapping(path = "/requests")
-public class ItemRequestController {
-}
