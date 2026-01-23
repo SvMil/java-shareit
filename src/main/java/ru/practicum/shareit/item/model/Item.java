f7fb298ad0d07e3f85931bb0b89592903e13c9cc
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2026-01-23 14:02:55 +0300

    fix add-controllers

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,10 +1,10 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Item {
     private Long id;
     private String name;
