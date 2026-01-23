d0120809c558bc1da73a10622851c2d7981255de
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2026-01-23 13:25:50 +0300

    fix add-controllers

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,10 +1,8 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(of = {"id"})
 public class Item {
     private Long id;
     private String name;
