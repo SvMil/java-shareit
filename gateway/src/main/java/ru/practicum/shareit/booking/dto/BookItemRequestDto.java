7f76fd084060ca4de484ef08d2a3d8b8c1dcb110
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2026-02-16 23:42:15 +0300

    add branch add-item-requests-and-gateway

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.booking.dto;
 
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -14,8 +15,10 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BookItemRequestDto {
     private long itemId;
+    @NotNull
     @FutureOrPresent
     private LocalDateTime start;
+    @NotNull
     @Future
     private LocalDateTime end;
 }
